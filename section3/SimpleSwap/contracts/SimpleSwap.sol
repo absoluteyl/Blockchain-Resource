c2ac986d9aa472335df352e6b9e019993ec31002
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-05-20 09:07:06 +0800

    add getReserve function for test_getReserve_should_be_able_to_get_Reserve
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

--- section3/SimpleSwap/contracts/SimpleSwap.sol
+++ section3/SimpleSwap/contracts/SimpleSwap.sol
@@ -8,8 +8,19 @@ contract SimpleSwap is ISimpleSwap, ERC20 {
     address public tokenA;
     address public tokenB;
 
+    uint112 private reserveA;
+    uint112 private reserveB;
+
     constructor(address _tokenA, address _tokenB) {
         tokenA = _tokenA;
         tokenB = _tokenB;
+    }
+
+    /// @notice Get the reserves of the pool
+    /// @return _reserveA The reserve of tokenA
+    /// @return _reserveB The reserve of tokenB
+    function getReserves() external view returns (uint256 _reserveA, uint256 _reserveB){
+        _reserveA = reserveA;
+        _reserveB = reserveB;
     }
 }
