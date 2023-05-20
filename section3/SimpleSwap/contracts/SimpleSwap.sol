d79334eadabadacedebc31200920d3a8bf7d53f6
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-05-20 09:03:11 +0800

    add tokenA, tokenB address and simple constructor to make SimpleSwapSetUp helper pass
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

--- section3/SimpleSwap/contracts/SimpleSwap.sol
+++ section3/SimpleSwap/contracts/SimpleSwap.sol
@@ -5,5 +5,11 @@ import { ISimpleSwap } from "./interface/ISimpleSwap.sol";
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract SimpleSwap is ISimpleSwap, ERC20 {
-    // Implement core logic here
+    address public tokenA;
+    address public tokenB;
+
+    constructor(address _tokenA, address _tokenB) {
+        tokenA = _tokenA;
+        tokenB = _tokenB;
+    }
 }
